ae063e98a486478ddefbe9b16a91b0eca4b7df74
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2024-09-23 00:00:56 +0300

    hesap makinesi v1 tamamlandı

---
+++ HesapMakinesiUygulamasi/v1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace v1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Kaç adet rakam toplamak istiyorsunuz ?");
+        int rakamAdedi = int.Parse(Console.ReadLine());
+
+        int rakamlarToplami = 0;
+
+        for (int i = 0; i < rakamAdedi; i++)
+        {
+            Console.WriteLine("Sayiyi giriniz: ");
+            int rakam = int.Parse(Console.ReadLine());
+
+            rakamlarToplami += rakam;
+        }
+
+        double rakamlarOrtalamasi = rakamlarToplami / rakamAdedi;
+
+        Console.WriteLine($"Toplam: {rakamlarToplami}");
+        Console.WriteLine($"Ortalama: {rakamlarOrtalamasi}");
+    }
+}
+
+/*
+
+v1
+- Kullanıcıya kaç adet rakam toplamak istediğini soralım.
+- Girdiği rakamlara göre toplam ve ortalama sonuç gösterelim.
+
+
+Cikti:
+    Kaç adet rakam toplamak istiyorsunuz ?
+    5
+    Sayiyi giriniz: 
+    1
+    Sayiyi giriniz: 
+    3
+    Sayiyi giriniz: 
+    5
+    Sayiyi giriniz: 
+    7
+    Sayiyi giriniz: 
+    9
+    Toplam: 25
+    Ortalama: 5
+*/
